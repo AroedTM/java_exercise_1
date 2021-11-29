b0358fc2b17af5935a6c2afe94d548fdc9920b1f
Enguerran Huby <ehuby@et.esiea.fr>
2021-11-29 16:01:01 +0100

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -9,8 +9,27 @@ public class Launcher
         String chaine = scanner.nextLine();
         while(!chaine.equals("quit"))
         {
-            System.out.println("Unknown command");
-            chaine = scanner.nextLine();
+            if(chaine.equals("fibo"))
+            {
+                System.out.println("Suite de Fibonacci. Entrer un nombre :");
+                int nombre = scanner.nextInt();
+                String retour = scanner.nextLine();
+                int F_0 = 0;
+                int F_1 = 1;
+                int F_X = 0;
+                for(int i=2; i<=nombre; i++)
+                {
+                    F_X = F_0 + F_1;
+                    F_0 = F_1;
+                    F_1 = F_X;
+                }
+                System.out.print("F(" + nombre + ") = " + F_X);
+                chaine = "quit";
+            }
+            else{
+                System.out.println("Unknown command");
+                chaine = scanner.nextLine();
+            }
         }
     }
 }
